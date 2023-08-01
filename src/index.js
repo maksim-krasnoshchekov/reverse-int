714e213b714c89448470058307efe1b5f6e282c4
Maksim <111878146+r-u-f-u-s@users.noreply.github.com>
2023-08-01 18:27:26 +0300

    init: feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-module.exports = function reverse (n) {
-}
+module.exports = function reverse(n) {
+    n = String(n);
+    let result = "";
+    for (let i = 0; i < n.length; i++) {
+        if (n[i] !== "-") {
+        result = n[i] + result;
+        } ;
+    };
+    return Number(result);
+};
